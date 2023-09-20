eba46abdd7b217358f785d4fc368ee4c022dac68
dongeranguk <rooetym@naver.com>
2023-09-21 00:21:37 +0900

    #4 - Home 화면 만들기
    Home 화면을 만들고, 더미 데이터를 생성하여 어떻게 출력되는지 확인하였다.

---
+++ lib/screen/main/tab/home/banks_dummy.dart
@@ -0,0 +1,8 @@
+import 'package:fast_app_base/screen/main/tab/home/vo/vo_bank.dart';
+
+import '../../../../common/constants.dart';
+
+
+final bankSinhan = Bank('신한은행', '$basePath/bank/bank_shinhan.png');
+final bankKakao = Bank('카카오뱅크', '$basePath/bank/bank_kakao.png');
+final bankTtoss = Bank('토스뱅크', '$basePath/bank/bank_toss.png');
