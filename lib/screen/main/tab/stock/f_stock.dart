0da4d608f0c6d9cfd0a8d67d1359bf3aab85df5d
dongeranguk <rooetym@naver.com>
2023-09-26 23:31:31 +0900

    #12 - 주식 탭 App Bar 생성
    주식 탭 `App Bar`를 만들어 보았다.

--- lib/screen/main/tab/stock/f_stock.dart
+++ lib/screen/main/tab/stock/f_stock.dart
@@ -1,4 +1,8 @@
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/screen/main/tab/stock/w_stock_app_bar.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../common/widget/w_image_button.dart';
 
 class StockFragment extends StatefulWidget {
   const StockFragment({super.key});
@@ -10,6 +14,32 @@ class StockFragment extends StatefulWidget {
 class _StockFragmentState extends State<StockFragment> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          backgroundColor: context.appColors.appBarBackground,
+          actions: [
+            ImageButton(
+              imagePath: '$basePath/icon/stock_search.png',
+              onTap: () {
+                context.showSnackbar("검색");
+              },
+            ),
+            ImageButton(
+              imagePath: '$basePath/icon/stock_calendar.png',
+              onTap: () {
+                context.showSnackbar("캘린더");
+              },
+            ),
+            ImageButton(
+              imagePath: '$basePath/icon/stock_settings.png',
+              onTap: () {
+                context.showSnackbar("설정");
+              },
+            ),
+          ],
+        ),
+      ],
+    ).color(Colors.black);
   }
 }
