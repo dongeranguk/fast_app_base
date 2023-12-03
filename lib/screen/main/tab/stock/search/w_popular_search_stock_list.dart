fbfecd40babea26664f3b72a4b30c7d6b77ff28d
dongeranguk <rooetym@naver.com>
2023-12-03 23:59:16 +0900

    #21 - animations 패키지를 사용한 애니메이션 구현
    `animations` 패키지를 사용하여 애니메이션을 구현해 보았다.
    
    실제 코드를 살펴보고 앱 내에 어떤 부분에 적용할 지 정한 뒤, 사용하고자 할 위젯으로 감싸주기만 한 것 만으로도 쉽게 애니메이션을 구현할 수 있었다.

--- lib/screen/main/tab/stock/search/w_popular_search_stock_list.dart
+++ lib/screen/main/tab/stock/search/w_popular_search_stock_list.dart
@@ -1,5 +1,7 @@
+import 'package:animations/animations.dart';
 import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/common/dart/extension/datetime_extension.dart';
+import 'package:fast_app_base/screen/main/tab/stock/search/s_stock_detail.dart';
 import 'package:fast_app_base/screen/main/tab/stock/search/vo/vo_popular_stock_dummy.dart';
 import 'package:fast_app_base/screen/main/tab/stock/search/w_popular_stock_item.dart';
 import 'package:flutter/material.dart';
@@ -17,10 +19,32 @@ class PopularSearchStockList extends StatelessWidget {
             emptyExpanded,
             '오늘 ${DateTime.now().formattedTime} 기준'.text.white.size(12).make(),
           ],
-        ),
+        ).pSymmetric(h: 20, v: 15),
         height20,
-        ...popularStockList.mapIndexed((element, index) => PopularStockItem(stock: element, rank: index+1)).toList(),
+        ...popularStockList
+            .mapIndexed((element, index) => OpenContainer<bool>(
+                  openColor: context.appColors.roundedLayoutBackground,
+                  closedColor: context.appColors.roundedLayoutBackground,
+                  openBuilder: (BuildContext context, VoidCallback _) {
+                    return StockDetail(element.name);
+                  },
+                  closedBuilder: (BuildContext context, VoidCallback action) {
+                    return Row(
+                      children: [
+                        (index + 1).text.bold.white.size(16).make(),
+                        width20,
+                        element.name.text.bold.white.size(16).make(),
+                        const Spacer(),
+                        element.todayPercentageString.text
+                            .color(element.getPriceColor(context))
+                            .size(16)
+                            .make()
+                      ],
+                    ).pSymmetric(h: 20, v: 20);
+                  },
+                ))
+            .toList()
       ],
-    ).pSymmetric(h: 20);
+    );
   }
 }
