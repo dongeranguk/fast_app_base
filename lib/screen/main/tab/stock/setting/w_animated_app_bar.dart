0fbc3d4ecfadcb5c4e89225e0644458cc99a5570
dongeranguk <rooetym@naver.com>
2023-12-07 23:20:36 +0900

    #21 - 커스텀 암시적 애니메이션을  구현한 애니메이션
    미리 구현된 암시적 애니메이션을 사용하여 애니메이션을 구현할 수 도 있지만, 원하는 애니메이션 효과를 내기 위해 직접 구현한 커스텀 애니메이션을 만들 수 있다.

--- lib/screen/main/tab/stock/setting/w_animated_app_bar.dart
+++ lib/screen/main/tab/stock/setting/w_animated_app_bar.dart
@@ -64,6 +64,26 @@ class _AnimatedAppBarState extends State<AnimatedAppBar> {
                   style: TextStyle(fontSize: getValue(30, 18)),
                   child: widget.title.text.white.make(),
                 )),
+            Positioned.fill(
+                child: Align(
+                    alignment: Alignment.topRight,
+                    child: TweenAnimationBuilder<Color?>(
+                      duration: 1000.ms,
+                      tween: ColorTween(
+                          begin: Colors.green,
+                          end: isTriggered ? Colors.orange : Colors.green),
+                      builder: (context, value, child) => ColorFiltered(
+                        colorFilter: ColorFilter.mode(
+                          value ?? Colors.green,
+                          BlendMode.modulate,
+                        ),
+                        child: child,
+                      ),
+                      child: Image.asset(
+                        '$basePath/icon/map_point.png',
+                        height: 60,
+                      ),
+                    )))
           ],
         ),
       ),
